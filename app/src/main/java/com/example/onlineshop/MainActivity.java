bc318e22c8c46ee6df72112530e465f30f0772ed
SaharOladi <sahar.olady@gmail.com>
2020-12-13 22:30:09 +0330

    Add BottomNavigationView in MainActivity. Add HomeFragment, CategoryFragment, ShoppingFragment and UserFragment.

--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.onlineshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -16,5 +22,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.fragment_container, HomeFragment.newInstance())
+                    .commit();
+        }
+
+        moveNavigationBottom();
     }
+
+    private void moveNavigationBottom() {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_shoppingBag:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, ShoppingFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+                                .commit();
+                        return true;
+                    case R.id.navigation_profile:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, UserFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+                                .commit();
+                        return true;
+                    case R.id.navigation_category:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, CategoryFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+
+                                .commit();
+                        return true;
+
+                    case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, HomeFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+
+                                .commit();
+                        return true;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                }
+            }
+        });
+    }
+
+
+    private void findViews() {
+        mBottomNavigationView = findViewById(R.id.navigation_button);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/onlineshop/ShoppingFragment.java
@@ -0,0 +1,36 @@
+package com.example.onlineshop;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShoppingFragment extends Fragment {
+
+
+    public ShoppingFragment() {
+        // Required empty public constructor
+    }
+
+    public static ShoppingFragment newInstance() {
+        ShoppingFragment fragment = new ShoppingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shopping, container, false);
+    }
+}
