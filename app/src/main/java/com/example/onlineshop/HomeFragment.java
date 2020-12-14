40d9172e68c1ca967adb5b39da5db6560ed4789e
SaharOladi <sahar.olady@gmail.com>
2020-12-14 18:34:45 +0330

    Add SliderAdapter and layout.

--- app/src/main/java/com/example/onlineshop/HomeFragment.java
+++ app/src/main/java/com/example/onlineshop/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.onlineshop;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,9 @@ import android.widget.TextView;
 import com.example.onlineshop.model.ImagesItem;
 import com.example.onlineshop.model.ProductsItem;
 import com.example.onlineshop.repository.Repository;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +28,12 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private static final int SPAN_COUNT = 3;
     public static final String TAG = "HomeFragment";
 
-    private RecyclerView mRecyclerView;
-    private List<ProductsItem> mProductsItems;
+    private List<ProductsItem> mProductsItems = new ArrayList<>();
     private List<ImagesItem> mImagesItems = new ArrayList<>();
+    private SliderView mSliderView;
+    private SliderAdapter mSliderAdapter;
     private Repository mRepository;
 
 
@@ -53,9 +57,13 @@ public class HomeFragment extends Fragment {
         mRepository.fetchAllProductItemsAsync(new Repository.Callbacks() {
             @Override
             public void onItemResponse(List<ProductsItem> items) {
-                setupAdapter(items);
-                mImagesItems.addAll(items.get(0).getImages());
-                Log.d(TAG, "onItemResponse: " + mImagesItems.size());
+
+                mProductsItems = items;
+                Log.d(TAG, "onItemResponse: "+mProductsItems.size());
+                mImagesItems = items.get(0).getImages();
+                Log.d(TAG, "onItemResponse: "+mImagesItems.size());
+
+
             }
         });
     }
@@ -73,67 +81,21 @@ public class HomeFragment extends Fragment {
     }
 
     private void findViews(View view) {
-        mRecyclerView = view.findViewById(R.id.recycler_view_photo_gallery);
+        mSliderView = view.findViewById(R.id.imageSlider);
     }
 
     private void initViews() {
-        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), SPAN_COUNT));
+        mSliderAdapter = new SliderAdapter(getContext(),mImagesItems);
+
+        mSliderView.setSliderAdapter(mSliderAdapter);
+
+        mSliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using IndicatorAnimationType. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        mSliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        mSliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        mSliderView.setIndicatorSelectedColor(Color.WHITE);
+        mSliderView.setIndicatorUnselectedColor(Color.GRAY);
+        mSliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        mSliderView.startAutoCycle();
     }
 
-    private void setupAdapter(List<ProductsItem> items) {
-        PhotoAdapter adapter = new PhotoAdapter(items);
-        mRecyclerView.setAdapter(adapter);
-    }
-
-    private class PhotoHolder extends RecyclerView.ViewHolder {
-
-        private TextView mTextView;
-
-        public PhotoHolder(@NonNull View itemView) {
-            super(itemView);
-
-            mTextView = itemView.findViewById(R.id.text);
-        }
-
-        public void bindItem(ProductsItem item) {
-            mTextView.setText(item.getDescription());
-        }
-    }
-
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
-
-        private List<ProductsItem> mItems;
-
-        public List<ProductsItem> getItems() {
-            return mItems;
-        }
-
-        public void setItems(List<ProductsItem> items) {
-            mItems = items;
-        }
-
-        public PhotoAdapter(List<ProductsItem> items) {
-            mItems = items;
-        }
-
-        @NonNull
-        @Override
-        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(getContext()).inflate(
-                    R.layout.row_item_test,
-                    parent,
-                    false);
-            return new PhotoHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
-            holder.bindItem(mItems.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mItems.size();
-        }
-    }
 }
---
+++ app/src/main/java/com/example/onlineshop/SliderAdapter.java
@@ -0,0 +1,70 @@
+package com.example.onlineshop;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.onlineshop.model.ImagesItem;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SliderAdapter extends SliderViewAdapter<SliderAdapter.SliderAdapterVH> {
+
+    private Context mContext;
+    private List<ImagesItem> mImagesItems = new ArrayList<>();
+
+    public SliderAdapter(Context context, List<ImagesItem> imagesItems) {
+        mContext = context;
+        mImagesItems = imagesItems;
+    }
+
+    @Override
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.slider_row_item, null);
+        return new SliderAdapterVH(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(SliderAdapterVH viewHolder, final int position) {
+
+        ImagesItem imagesItem = mImagesItems.get(position);
+
+        viewHolder.textViewDescription.setText(imagesItem.getName());
+        viewHolder.textViewDescription.setTextSize(16);
+        viewHolder.textViewDescription.setTextColor(Color.WHITE);
+        Glide.with(viewHolder.itemView)
+                .load(imagesItem.getSrc())
+                .fitCenter()
+                .into(viewHolder.imageViewBackground);
+    }
+
+    @Override
+    public int getCount() {
+        //slider view count could be dynamic size
+        return mImagesItems.size();
+    }
+
+    class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        View itemView;
+        ImageView imageViewBackground;
+        ImageView imageGifContainer;
+        TextView textViewDescription;
+
+        public SliderAdapterVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            imageGifContainer = itemView.findViewById(R.id.iv_gif_container);
+            textViewDescription = itemView.findViewById(R.id.tv_auto_image_slider);
+            this.itemView = itemView;
+        }
+    }
+
+}
